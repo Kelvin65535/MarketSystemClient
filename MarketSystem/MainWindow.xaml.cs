24ca9f3b4b7518b5e17e0d69f702eadf30a8b2e6
叶嘉永 <kelvin65535@gmail.com>
2017-05-10 16:45:50 +0800

    修改主界面显示，增加控件的数据绑定，新增商品管理类

--- MarketSystem/MainWindow.xaml.cs
+++ MarketSystem/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,79 @@ namespace MarketSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //局部变量
+        /// <summary>
+        /// ListView数据源，存放商品item的集合
+        /// </summary>
+        private ObservableCollection<ShopItem> itemList;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitListViewDataBindings();
+        }
+
+        /// <summary>
+        /// 为ListView绑定数据源
+        /// </summary>
+        private void InitListViewDataBindings()
+        {
+            //为ListView绑定数据源
+            itemList = new ObservableCollection<ShopItem>();
+            listviewShopItem.ItemsSource = itemList;
+            
+        }
+
+        private void tb商品编号_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                //获取输入的商品编号
+                string num = tb商品编号.Text;
+                //判断商品是否存在list中，若存在则数量+1
+                foreach (var item in itemList)
+                {
+                    if (item.ItemNum == num)
+                    {
+                        item.ItemCount++;
+                        更新合计价格();
+                        //清除输入
+                        tb商品编号.Text = null;
+                        return;
+                    }
+                }
+                //为itemList添加新的商品item
+                //TODO 使用ajax获取商品信息
+                itemList.Add(new ShopItem(num, "测试商品", 1, 1.00, 5.00));
+                更新合计价格();
+                //清除输入
+                tb商品编号.Text = null;
+            }
+        }
+
+        /// <summary>
+        /// 主窗口加载完成后执行以下方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //将键盘焦点聚焦到tb商品编号文本框
+            Keyboard.Focus(tb商品编号);
+        }
+
+        /// <summary>
+        /// 更新主界面的“合计价格”标签
+        /// </summary>
+        private void 更新合计价格()
+        {
+            double 合计价格 = 0.00;
+            foreach (var item in itemList)
+            {
+                合计价格 += (item.ItemCount * item.ItemSellPrice);
+            }
+            label合计价格.Content = 合计价格.ToString();
         }
     }
 }
---
+++ MarketSystem/ShopItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSystem
+{
+    class ShopItem : INotifyPropertyChanged
+    {
+        //商品编码
+        private string itemNum;
+        //商品名称
+        private string itemName;
+        //商品数量
+        private int itemCount;
+        //商品原价
+        private double itemOriginalPrice;
+        //商品折后价
+        private double itemSellPrice;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //封装字段
+        public string ItemNum {
+            get => itemNum;
+            set {
+                itemNum = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ItemNum"));
+            }
+        }
+        public string ItemName { get => itemName; set => itemName = value; }
+        public int ItemCount {
+            get => itemCount;
+            set {
+                itemCount = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ItemCount"));
+            }
+        }
+        public double ItemOriginalPrice { get => itemOriginalPrice; set => itemOriginalPrice = value; }
+        public double ItemSellPrice { get => itemSellPrice; set => itemSellPrice = value; }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+        //构造方法
+        public ShopItem(string itemNum, string itemName, int itemCount, double itemOriginalPrice, double itemSellPrice)
+        {
+            this.itemNum = itemNum;
+            this.itemName = itemName;
+            this.itemCount = itemCount;
+            this.itemOriginalPrice = itemOriginalPrice;
+            this.itemSellPrice = itemSellPrice;
+        }
+    }
+}
