569eb15a8580a623f34fc5dcbb2237290a9106b9
叶嘉永 <kelvin65535@gmail.com>
2017-05-10 20:46:17 +0800

    增加主界面的快捷键机制

--- MarketSystem/MainWindow.xaml.cs
+++ MarketSystem/MainWindow.xaml.cs
@@ -28,10 +28,25 @@ namespace MarketSystem
         /// </summary>
         private ObservableCollection<ShopItem> itemList;
 
+        /// <summary>
+        /// 存放搁置订单的订单列表
+        /// </summary>
+        private ObservableCollection<ObservableCollection<ShopItem>> 搁置订单列表;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitData();
             InitListViewDataBindings();
+        }
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        private void InitData()
+        {
+            label搁置订单.Content = 0;
+            搁置订单列表 = new ObservableCollection<ObservableCollection<ShopItem>>();
         }
 
         /// <summary>
@@ -42,7 +57,6 @@ namespace MarketSystem
             //为ListView绑定数据源
             itemList = new ObservableCollection<ShopItem>();
             listviewShopItem.ItemsSource = itemList;
-            
         }
 
         private void tb商品编号_KeyUp(object sender, KeyEventArgs e)
@@ -51,6 +65,10 @@ namespace MarketSystem
             {
                 //获取输入的商品编号
                 string num = tb商品编号.Text;
+                if (num == "") //输入为空，不执行操作
+                {
+                    return;
+                }
                 //判断商品是否存在list中，若存在则数量+1
                 foreach (var item in itemList)
                 {
@@ -70,6 +88,8 @@ namespace MarketSystem
                 //清除输入
                 tb商品编号.Text = null;
             }
+            
+            
         }
 
         /// <summary>
@@ -94,6 +114,123 @@ namespace MarketSystem
                 合计价格 += (item.ItemCount * item.ItemSellPrice);
             }
             label合计价格.Content = 合计价格.ToString();
+        }
+        
+        /// <summary>
+        /// 检测输入到商品编号中指定的热键并执行功能
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tb商品编号_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || e.Key == Key.Enter)
+            {
+                return;
+            }
+
+            switch (e.Key)
+            {
+                //当检测到s键输入时将当前订单搁置
+                case Key.S:
+                    新增搁置订单();
+                    tb商品编号.Text = null;
+                    e.Handled = true;
+                    break;
+                //当检测到r键输入时将当前搁置订单还原
+                case Key.R:
+                    还原搁置订单();
+                    tb商品编号.Text = null;
+                    e.Handled = true;
+                    break;
+                //当检测到“向下”按钮输入时跳转到商品列表进行操作
+                case Key.Down:
+                    tb商品编号.Text = "";
+                    if (listviewShopItem.Items.Count == 0)
+                    {
+                        break;
+                    }
+                    listviewShopItem.SelectedIndex = 0;
+                    Keyboard.Focus(listviewShopItem);
+                    break;
+                //c键输入，清除商品列表
+                case Key.C:
+                    tb商品编号.Text = null;
+                    itemList.Clear();
+                    break;
+                //退格按钮
+                case Key.Back:
+                    break;
+                //其他按钮无视操作
+                default:
+                    tb商品编号.Text = "";
+                    e.Handled = true;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 新增搁置订单到搁置列表中，并清除当前内容
+        /// </summary>
+        private void 新增搁置订单()
+        {
+            if (listviewShopItem.Items.Count == 0)
+            {
+                return;
+            }
+            int 搁置订单数 = 搁置订单列表.Count;
+            //var temp = new ObservableCollection<ShopItem>(itemList.ToArray());
+            搁置订单列表.Add(new ObservableCollection<ShopItem>(itemList.ToArray()));
+            搁置订单数++;
+            label搁置订单.Content = 搁置订单数;
+            itemList.Clear();
+        }
+
+        /// <summary>
+        /// 还原搁置订单到搁置列表中
+        /// </summary>
+        private void 还原搁置订单()
+        {
+            if (搁置订单列表.Count == 0)
+            {
+                return;
+            }
+            int 搁置订单数 = 搁置订单列表.Count;
+            itemList.Clear();
+            ObservableCollection<ShopItem> temp = 搁置订单列表.Last();
+            foreach (var item in temp)
+            {
+                itemList.Add(item);
+            }
+            搁置订单列表.RemoveAt(搁置订单数 - 1);
+            搁置订单数--;
+            label搁置订单.Content = 搁置订单数;
+        }
+        
+
+        private void listviewShopItem_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //当检测到按下数字键时自动切换回输入编号窗口
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                Keyboard.Focus(tb商品编号);
+            }
+
+            if (e.Key == Key.Left)
+            {
+
+            }
+        }
+
+        private void ListViewItem_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    MessageBox.Show("test");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
